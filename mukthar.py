6d8635f49e7e8d88983a65e62899fd372ba4125e
Mukthar <mukthar.mecp@gmail.com>
2025-05-05 10:54:13 +0530

    Initial commit

---
+++ mukthar.py
@@ -0,0 +1,10 @@
+# Sample Python code to print Fibonacci sequence
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=' ')
+        a, b = b, a + b
+
+# Example: Print first 10 Fibonacci numbers
+fibonacci(10)
